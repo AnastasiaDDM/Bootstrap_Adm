99a3cff04df3a95b2578a38b15c0b2c0c48b0b76
AnastasiaDDM <ndmitrievnaa@mail.ru>
2020-04-09 16:06:03 +0800

    09.04.2020 16:06:03,29

--- js/javascript.js
+++ js/javascript.js
@@ -49,13 +49,130 @@ function areas_more() {
   }
 
  
+
+//   function button_search() {
+// 	console.log($("[href='#find_form']"));
+// 	$("[href='#find_form']").click(function() {
+
+// 	  if ($( "#find_form" ).attr('class') == 'show' ) {
+// 		$("[href='#find_form']").html( "Cвернуть поиск" );
+// 		$(this).removeClass("btn-outline-info");
+// 		$("[href='#find_form']").addClass("btn-outline-secondary");
+// 	  }
+// 	  else {
+// 		$( this ).html( "Расширенный поиск" );
+// 		$(this).removeClass("btn-outline-secondary");
+// 		$("[href='#find_form']").addClass("btn-outline-info");
+// 	  }
+
+// 	});
+//   }
+
+
+  function search_roll() {  
+    $("[href='#find_form']").click(function() {
+	  if ($( "#find_form" ).attr('style') == 'display: none;' ) {
+		$( "#find_form" ).slideDown( "slow", function() {
+			$( "[href='#find_form']" ).html( "Cвернуть поиск" );
+			$("[href='#find_form']").removeClass("btn-outline-info");
+			$( "[href='#find_form']" ).addClass("btn-outline-secondary");
+		});	
+	  }
+	  else {
+		  $( "#find_form" ).slideUp( "slow", function() {
+			$( "[href='#find_form']" ).html( "Расширенный поиск" );
+			$("[href='#find_form']").removeClass("btn-outline-secondary");
+			$( "[href='#find_form']" ).addClass("btn-outline-info");
+		  });
+	  }
+    });
+}
+
+
+
+
+function table_append_newworker()
+{
+
+  $("[href='#newworker']").click( function () {
+
+	//   $(".btn_answer").show(500);
+	//   $(this).hide(500);
+	//   $("[data-rel='single_form_answer']").hide(500);
+	//   $("[data-rel='single_form_answer']").detach();
+
+
+	//  ВОТ ЗДЕСЬ ПРОБЛЕМА ВОЗНИКАЕТ. ПОСМОТРИ В КОНСОЛИ БРАУЗЕРА - ТАМ НЕ ВЫВОДЯТСЯ ТЕГИ TR TD
+	console.log($( "#pattern_table_tr_newworker").html());
+	  var newworker = $( "#pattern_table_tr_newworker").html();
+
+	  console.log($("#table_workers>tbody tr:last-child"));
+	  $('#table_workers>tbody tr:last-child').parent().append(newworker);
+
+	//   $(this).parent().append( $(form) );
+
+	  // console.log($(this).siblings(".copy_form_answer"));
+
+	  $($(this).siblings(".copy_tr_newworker").attr( "data-rel", "single_tr_newworker" ));
+
+	  $("[data-rel='single_tr_newworker']").show(500);
+
+	//   activate_form_answer();
+	});
+
+	// $( ".btn_answer_two" ).click(function () {
+
+	// 	$(".btn_answer_two").show(500);
+	// 	$(this).hide(500);
+
+	// 	$("[data-rel='single_form_answer']").hide(500);
+	// 	$("[data-rel='single_form_answer']").detach();
+
+	// 	// var bt_data = $(this).data("target");
+
+	// 	var form = $( "#pattern_form_answer").html(); 
+
+	// 	$(this).parents(".discussion").append( $(form) );
+
+	// 	$($( ".discussion" ).children( ".copy_form_answer" ).attr( "data-rel", "single_form_answer" ));
+
+	// 	$("[data-rel='single_form_answer']").show(500);
+
+	// 	activate_form_answer();
+	// });
+
+	// $( ".comment_roll" ).click(function() {
+
+	//   var com_data = $(this).data("target");
+
+	//   $( "[id="+com_data+"] .discussion_comment" ).slideToggle( "slow", function() {
+
+	//   });
+	// });
+
+	// $( ".btn_rollup" ).click(function() {
+	  
+	//   $( this ).parents(".discussion_comment").slideToggle( "slow", function() {
+	//   });
+	// });
+
+}
+
+
+
+
+
+
+
+
 jQuery(document).ready(function () {
 	
 	password_roll();
 	areas_more();
 	input_file();
-	textarea_filled();
 	search_roll();
+	// button_search();
+	table_append_newworker();
 	
 	if ($( window ).width() < 600) {
 		$('.search_datepicker').each(function() {
