81820cf179cce4eb4bb9e71cd3b8d42d9de467f4
AnastasiaDDM <ndmitrievnaa@mail.ru>
2020-04-11 14:59:33 +0800

    11.04.2020 14:59:33,10

--- js/javascript.js
+++ js/javascript.js
@@ -48,26 +48,6 @@ function areas_more() {
 	});
   }
 
- 
-
-//   function button_search() {
-// 	console.log($("[href='#find_form']"));
-// 	$("[href='#find_form']").click(function() {
-
-// 	  if ($( "#find_form" ).attr('class') == 'show' ) {
-// 		$("[href='#find_form']").html( "Cвернуть поиск" );
-// 		$(this).removeClass("btn-outline-info");
-// 		$("[href='#find_form']").addClass("btn-outline-secondary");
-// 	  }
-// 	  else {
-// 		$( this ).html( "Расширенный поиск" );
-// 		$(this).removeClass("btn-outline-secondary");
-// 		$("[href='#find_form']").addClass("btn-outline-info");
-// 	  }
-
-// 	});
-//   }
-
 
   function search_roll() {  
     $("[href='#find_form']").click(function() {
@@ -91,120 +71,60 @@ function areas_more() {
 
 
 
+
+
+
+//Ф-ия добавления новой строки в таблицу разработчиков
 function table_append_newworker()
 {
-  $("#add_new_worker").click( function () {
-  var newworker = $( "#pattern_table_tr_newworker");
-  console.log( newworker);
+	$("#add_new_worker").click( function () {
+	var newworker = $( "#pattern_table_tr_newworker");
+	console.log( newworker);
 
-  var next_id=1;
-  if ($( "#table_workers").children().length != 0)
-  {
+
+	// Находим максимальное кол-во строк в таблице 
+	var next_id=1;
+	if ($( "#table_workers").children().length != 0)
+	{
 	// var maxid =  parseInt($( "#table_workers tr:last-child>[id='row_worker_id']").html());
 	var maxid =  parseInt($( "#table_workers tr:last-child>th").html());
-    next_id = maxid + 1;
-  }
-  $('#table_workers').append("<tr style='display:none;' id=worker_tr_"+next_id+">"+newworker.html()+"</tr>");
+	next_id = maxid + 1;
+	}
+
+	// Вставляем новую строку
+	$('#table_workers').append("<tr style='display:none;' id=worker_tr_"+next_id+">"+newworker.html()+"</tr>");
+
+	// Вставляем номер строки в 1 колонку
+	$("[id='worker_tr_"+next_id+"'] th").html(next_id);
+	//   $("#table_workers tr:last-child>th").html(next_id);
 
 
-$("[id='worker_tr_"+next_id+"'] th").html(next_id);
-//   $("#table_workers tr:last-child>th").html(next_id);
-
-
-//   $("#table_workers tr:last-child").slideDown("slow");
-  $("#table_workers tr:last-child").show("slow");
-  table_tr_worker_remove();
-  });
-  table_tr_worker_remove();
+	// Показываем добавленную строку
+	//   $("#table_workers tr:last-child").slideDown("slow");
+	$("#table_workers tr:last-child").show("slow");
+	table_tr_worker_remove();
+	});
+	table_tr_worker_remove();
 }
 
 
-
-
-
-
-
-// function table_append_newworker()
-// {
-
-//   $("[href='#newworker']").click( function () {
-
-// 	var target = $("#table_workers tr:last-child>[data-id='row_worker_id']");
-
-
-// 	  var newworker = $( "#pattern_table_tr_newworker").html();
-
-// 	//   if ($( "#table_workers>tbody tr:last-child>[data-id='row_worker_id']").html() == "undefined")
-// 	//   {
-// 	// 	//   var maxid = 0;
-// 	// 	  var next_id = 1;
-// 	//   }
-// 	//   else {
-// 	// 	var maxid =  parseInt($( "#table_workers>tbody tr:last-child>[data-id='row_worker_id']").html());
-// 	// 	var next_id = maxid + 1;
-// 	//   }
-
-
-
-// 	if (target.html() == "undefined")
-// 	{
-// 	  //   var maxid = 0;
-// 		var next_id = 1;
-// 	}
-// 	else {
-// 	  var maxid =  parseInt($( "#table_workers>tbody tr:last-child>[data-id='row_worker_id']").html());
-// 	  var next_id = maxid + 1;
-// 	}
-
-
-
-	  
-
-
-
-// 		   console.log($( "#table_workers>tbody tr:last-child>[data-id='row_worker_id']").html());
-// 		   console.log( target);
-
-// 	  $('#table_workers>tbody tr:last-child').parent().append(newworker);
-
-// 	  $('#table_workers>tbody tr:last-child').attr( "id", "worker_tr_"+next_id );
-
-// 	  $('#table_workers tr:last-child>td:last-child button').attr( "data-worker_remove", "worker_tr_"+next_id );
-
-// 	  $('#table_workers>tbody tr:last-child th').html(next_id);
-
-// 	  console.log( $('#table_workers>tbody tr:last-child th').html(next_id));
-
-// 	//   $(this).parent().append( $(form) );
-
-// 	  // console.log($(this).siblings(".copy_form_answer"));
-
-// 	  $(".copy_tr_newworker").attr( "data-rel", "single_tr_newworker" );
-
-// 	  $("#table_workers>tbody tr:last-child").show("slow");
-
-// 	  table_remove_tr_worker();
-// 	//   activate_form_answer();
-// 	});
-
-// 	table_remove_tr_worker();
-
-// }
-
-
+//Ф-ия удаления строки в таблицу разработчиков
 function table_tr_worker_remove()
 {
   var row_id=0;
 
   $("#table_workers [data-toggle='modal']").click( function () {
 
-  row_id = $(this).parent().parent().parent().attr('id');
-//   console.log($("#table_workers [data-toggle='modal']").html());
-  console.log(row_id);
+	// Получаем номер строки
+	row_id = $(this).parent().parent().parent().attr('id');
+	//   console.log($("#table_workers [data-toggle='modal']").html());
+	console.log(row_id);
   });
 
+  // Нажатие на кнопку подтверждение
   $("#btn_del_confirm").click( function () {
 
+	// Удаление строки
 	// $("#"+row_id).slideUp("slow");
 	$("#"+row_id).remove();	
 
@@ -216,75 +136,12 @@ function table_tr_worker_remove()
 
 
 
-
-// function table_remove_tr_worker()
-// {
-
-
-
-// 	// $('.btn-add-name-company').on('click', function () {
-// 	// 	var name = $('.modal').find('inpit[name="name"]').val();
-// 	// 	//Делайте с ним то, что нужно
-// 	//   });
-	
-
-
-//   $("#table_workers td:last-child button").click( function () {
-
-// 	// console.log( $("#table_workers td:last-child button"));
-
-// 	var row_id = $(this).attr("data-worker_remove");
-// 	console.log(row_id);
-
-// 	// $("#table_workers>tbody tr [id="+row_id+"]").remove();
-
-// 	// $("[id='worker_tr_2']").remove();
-
-// 	$("#ModalDel #btn_confirm").click( function () {
-
-// 		console.log($("#table_workers tr[id="+row_id+"]").html());
-
-
-// 		$("#table_workers tr[id="+row_id+"]").remove();
-
-// 		// $("#ModalDel").on("dialogclose", function(){});
-// 		$("#ModalDel").modal("hide");
-
-// 	})
-
-// 	});
-
-
-
-// }
-
-
-
-// function table_del_tr_image()
-// {
-//   var row_id=0;
-
-//   $("#table_images [data-toggle='modal']").click( function () {
-
-//   row_id = $(this).parent().parent().parent().attr('id');
-// //   console.log($("#table_workers [data-toggle='modal']").html());
-//   console.log(row_id);
-//   });
-
-//   $("#btn_del_confirm").click( function () {
-
-// 	// $("#"+row_id).slideUp("slow");
-// 	$("#"+row_id).remove();	
-
-//     $("#ModalDel").modal("hide");
-
-//   });
-
-// }
-
+//Ф-ия добавления новой строки в таблицу изображений 
 function tr_image_add( img_name, img_path) {
-
+	// Копирование шаблона строки изображения
 	var newimage = $( "#pattern_table_tr_newimage");
+
+	// Получения максимального кол-ва строк в таблице
 	var next_id=1;
 	if ($( "#table_images").children().length != 0)
 	{
@@ -292,13 +149,15 @@ function tr_image_add( img_name, img_path) {
 	  next_id = maxid + 1;
 	}
 
-
+	// Вставка строки в таблицу
 	$('#table_images').append("<tr  id=image_tr_"+next_id+">"+newimage.html()+"</tr>");
 
 	var current_tr = $("[id='image_tr_"+next_id+"']");
 	console.log(current_tr);
 	console.log($(current_tr).html());
 
+
+	//Здесь я хотела добавлять данные в строку по средствам переменной current_tr, но не получилось
 
 	// $(current_tr+" th").html(next_id);
 	// // $(current_tr+"img").attr( "src", img_path );
@@ -308,13 +167,13 @@ function tr_image_add( img_name, img_path) {
 	// $("#ModalSuccess_Add").modal("show");
 	// console.log('"'+current_tr+' th"');
 
+	// Заполнение строки данными, переданными извне
 	$("[id='image_tr_"+next_id+"'] th").html(next_id);
 	// $(current_tr+"img").attr( "src", img_path );
 	$("[id='image_tr_"+next_id+"'] img").attr({ "src": img_path, "alt": img_name });
 	$("[id='image_tr_"+next_id+"'] [data-img='image_name']").html(img_name);
 	$("[id='image_tr_"+next_id+"'] input").attr("id", "radoi_image_"+next_id);
 	$("[id='image_tr_"+next_id+"'] label").attr("for", "radoi_image_"+next_id);
-
 
 
 	$("#ModalSuccess_Add").modal("show");
@@ -325,20 +184,22 @@ function tr_image_add( img_name, img_path) {
 }
 
 
+//Ф-ия получения данных о новом изображении из модального окна для добавления
 function modal_tr_image_add()
 {
 
 	$("#add_new_image").click( function () {
 
+		//Корректировка отображения нужных кнопок формы
 		$("#btn_edit_image_confirm").hide();
 		$("#btn_add_image_confirm").show();
 		$("#ModalDownload").modal("show");
 
-	  
+	
 		$("#btn_add_image_cancel").click( function () {
-		  $('#modal_image_name').val('');
-		  $('#modal_image_path').val('');
-		});
+		$('#modal_image_name').val('');
+		$('#modal_image_path').val('');
+	});
 
 		// table_tr_image_edit();
 		// table_tr_image_remove();
@@ -347,30 +208,20 @@ function modal_tr_image_add()
 	
 	$("#btn_add_image_confirm").click( function () {
 
+		// Сохранение данных из полей формы
 		var img_name = $('#modal_image_name').val();
 	
 		var img_path = $('#modal_image_path').val();
 
+		// Закрытие формы и очистка полей
 		$("#ModalDownload").modal("hide");
 		// $('#ModalDownload')[0].reset();
 	
 		$('#modal_image_name').val('');
 		$('#modal_image_path').val('');
 	
-	
-	
-	
+		// Вызов ф-ии добавления новой информации в таблицу
 		tr_image_add(img_name, img_path);
-		
-		
-	
-		// $(current_tr+"th").html(next_id);
-		// // $(current_tr+"img").attr( "src", img_path );
-		// $(current_tr+"img").attr({ "src": img_path, "alt": img_name });
-		// $(current_tr+"[data-img='image_name']").html(img_name);
-	
-  
-  
 	});
 
 
@@ -383,7 +234,7 @@ function modal_tr_image_add()
 // неужели нельзя без этой белиберды??
 // просто взять и прописать все эти 3 функции написать
 // внизу где происходят вызовы всех основных функций в Jquery
-// я думала, может как-то в if взять что-нибудь, но так и не поняла как ( да и думаю это не поможет)
+// я думала, может как-то в if взять что-нибудь, но так и не поняла как ( да и думаю это не поможетв)
 
 	table_tr_image_edit();
 	table_tr_image_remove();
@@ -396,28 +247,32 @@ function modal_tr_image_add()
 
 
 
+//Ф-ия удаления строки изображения
 function table_tr_image_remove()
 {
-  var row_id=0;
+	var row_id=0;
 
-  $("#table_images [data-target='#ModalDel']").click( function () {
+	$("#table_images [data-target='#ModalDel']").click( function () {
 
-  row_id = $(this).parent().parent().parent().attr('id');
-  });
+		// Получение номера строки
+		row_id = $(this).parent().parent().parent().attr('id');
+	});
 
-  $("#btn_del_confirm").click( function () {
+	$("#btn_del_confirm").click( function () {
 
-	// $("#"+row_id).slideUp("slow");
-	$("#"+row_id).remove();	
+		// Удаление строки из таблицы
+		// $("#"+row_id).slideUp("slow");
+		$("#"+row_id).remove();	
 
-	$("#ModalDel").modal("hide");
-	$("#ModalSuccess_Del").modal("show");
+		$("#ModalDel").modal("hide");
+		$("#ModalSuccess_Del").modal("show");
 
-  });
+	});
 }
 
 
 
+//Ф-ия изменения данных строки в таблице изображений 
 function tr_image_edit( img_name, img_path, row_id) {
 
 	console.log(row_id);
@@ -425,6 +280,7 @@ function tr_image_edit( img_name, img_path, row_id) {
 	console.log(img_path);
 
 
+	// Заполнение строки таблицы новыми данными
 	// $("[id='image_tr_"+row_id+"'] th").html(row_id);
 	// $(current_tr+"img").attr( "src", img_path );
 	// $("#"+row_id+" img").attr({ "src": img_path, "alt": img_name });
@@ -437,50 +293,54 @@ function tr_image_edit( img_name, img_path, row_id) {
 }
 
 
-
+//Ф-ия получения изменненных данных об изображении из модального окна для редактирования
 function table_tr_image_edit()
 {
 	var row_id=0;
 
 	$("#table_images [data-edit='#ModalDownload']").click( function () {
 
+		//Корректировка отображения нужных кнопок формы
 		$("#btn_add_image_confirm").hide();
 		$("#btn_edit_image_confirm").show();
 		$("#ModalDownload").modal("show");
 
-		
 
-	row_id = $(this).parent().parent().parent().attr('id');
-	console.log("1111ddddd"+row_id);
+		// Получение номера строки
+		row_id = $(this).parent().parent().parent().attr('id');
 
-	$('#modal_image_name').val($("#"+row_id+" [data-img='image_name']").html());
-	//   $('#modal_image_path').val($("#"+row_id+" img").attr('src'));
-	
-	//   $("[id='image_tr_"+next_id+"'] img").attr({ "src": img_path, "alt": img_name });
-	//   $("[id='image_tr_"+next_id+"'] [data-img='image_name']").html(img_name);
+		// Заполнение формы данными
+		$('#modal_image_name').val($("#"+row_id+" [data-img='image_name']").html());
+		//   $('#modal_image_path').val($("#"+row_id+" img").attr('src'));
+
+		//   $("[id='image_tr_"+next_id+"'] img").attr({ "src": img_path, "alt": img_name });
+		//   $("[id='image_tr_"+next_id+"'] [data-img='image_name']").html(img_name);
 
 
-	console.log($("#"+row_id+" [data-img='image_name']").html());
-	  });
+		console.log($("#"+row_id+" [data-img='image_name']").html());
+	});
 	  
   
 	$("#btn_edit_image_confirm").click( function () {
   
-	  console.log($('#modal_image_name').val());
-	  var img_name = $('#modal_image_name').val();
-  
-	  var img_path = $('#modal_image_path').val();
-  
-	  $("#ModalDownload").modal("hide");
-  
-	  $('#modal_image_name').val('');
-	  $('#modal_image_path').val('');
-  
-	  console.log(row_id);
-	  console.log(img_name);
-	  console.log(img_path);
-  
-	  tr_image_edit(img_name, img_path, row_id);
+		// Получение данных из полей формы
+		console.log($('#modal_image_name').val());
+		var img_name = $('#modal_image_name').val();
+
+		var img_path = $('#modal_image_path').val();
+
+		// Закрытие и очистка модального окна
+		$("#ModalDownload").modal("hide");
+
+		$('#modal_image_name').val('');
+		$('#modal_image_path').val('');
+
+		console.log(row_id);
+		console.log(img_name);
+		console.log(img_path);
+
+		// Вызов ф-ии изменения информации в таблице
+		tr_image_edit(img_name, img_path, row_id);
   
 	});
 
@@ -489,31 +349,35 @@ function table_tr_image_edit()
 
 
 
-
+//Ф-ия назначения статуса главного изображения
 function table_image_checked_main()
 {
-  var input_id;
+	var input_id;
 
-  $("#table_images input").click( function () {
+	$("#table_images input").click( function () {
 
-  input_id = $(this);
-  $(this).prop("checked",false);
-  $(image_checked_input_id).prop("checked",true);
+		// Сохранение состояния, т.е. оставляем выбранным предыдущее изображение
+		input_id = $(this);
+		$(this).prop("checked",false);
+		$(image_checked_input_id).prop("checked",true);
 
-  $("#ModalMain").modal("show");
+		$("#ModalMain").modal("show");
 
-  });
+	});
 
-  $("#image_checked_main_confirm").click( function () {
+	$("#image_checked_main_confirm").click( function () {
 
-	$(input_id).prop("checked",true);
-	image_checked_input_id = input_id;
-	console.log(image_checked_input_id);
+		// Выбираем новое изображение главым
+		$(input_id).prop("checked",true);
+		
+		// Меняем значение глобальной переменной главного изображения
+		image_checked_input_id = input_id;
+		console.log(image_checked_input_id);
 
-	$("#ModalMain").modal("hide");
-	$("#ModalSuccess_Edit").modal("show");
+		$("#ModalMain").modal("hide");
+		$("#ModalSuccess_Edit").modal("show");
 
-  });
+	});
 }
 
 
@@ -526,7 +390,7 @@ function table_image_checked_main()
 
 
 
-
+// Глобальная переменная статуса главного изображения
 var image_checked_input_id;
 image_checked_input_id = $("#table_images input:checked");
 
